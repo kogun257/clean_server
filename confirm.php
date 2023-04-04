067bb5278de599f9128bbfd4c401e358c10bace3
kogun257 <69184724+kogun257@users.noreply.github.com>
2023-04-04 12:07:39 +0900

    Removed google api key

--- confirm.php
+++ confirm.php
@@ -189,7 +189,7 @@ require_once("./properties.php");
 
         }
       </script>
-      <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyAOhP3cXmIts-lg7K2T2HUDCKpGpUq8nrI&callback=initMap" async></script>
+      <script src="//maps.googleapis.com/maps/api/js?key==initMap" async></script>
     </div>
 
   <!--被写体の表示-->
